f2e9ec3c650071c6071477e269461a82f83a5373
Diego de Paula Borges <chsbdiego@msn.com>
2016-03-20 11:12:00 -0300

    Committing page for the first time.

---
+++ CSS_CV_files/myCV.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="jquery-2.0.2.js" />
+$(document).ready(init);
+
+function init() {
+    $(".hideable>header").on('click', clicked);
+    $("#expandall").on('click',expandAll);
+    $("#collapseall").on('click',collapseAll);
+
+}
+
+function expandAll() {
+    $(".hideable").removeClass("hidden");
+}
+function collapseAll() {
+    $(".hideable").addClass("hidden");
+}
+
+function clicked() {
+    var pp = $(this).parent("section");
+    if (pp.hasClass("hidden")) {
+        pp.removeClass("hidden");
+    } else {
+        pp.addClass("hidden");
+    }
+}
